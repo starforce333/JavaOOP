23563783899b405eece72a5b5f9dd36b0aa0b745
starforce333 <starforce@i.ua>
2018-11-13 15:20:20 +0200

    Домашнее задание 2 урока поправил

--- src/lesson02/home/Main.java
+++ src/lesson02/home/Main.java
@@ -10,26 +10,23 @@ public class Main {
 
         Round round = new Round(3, 8);
 
+        System.out.println("Square");
         System.out.print("Area of sqr: ");
         System.out.println(sqr.getArea());
-        System.out.println();
         System.out.print("Perimeter of sqr: ");
         System.out.println(sqr.getPerimetr());
         System.out.println();
-        System.out.println();
+        System.out.println("Triangle");
         System.out.print("Perimeter of tri: ");
         System.out.println(tri.getPerimetr());
-        System.out.println();
         System.out.print("Area of tri: ");
         System.out.println(tri.getArea());
         System.out.println();
-        System.out.println();
+        System.out.println("Round");
         System.out.print("Area of round: ");
         System.out.println(round.getArea());
-        System.out.println();
         System.out.print("Perimeter of round: ");
         System.out.println(round.getPerimetr());
-        System.out.println();
         System.out.println();
 
         Board blackboard = new Board();
@@ -37,8 +34,10 @@ public class Main {
         blackboard.putFigure(tri);
         blackboard.putFigure(sqr);
         blackboard.putFigure(sqr);
+
         blackboard.removeFigure(sqr);
         blackboard.removeFigure(tri);
+
         blackboard.putFigure(round);
         blackboard.putFigure(tri);
 
