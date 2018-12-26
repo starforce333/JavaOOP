03a92211131f45791701b1418aca9e8d627d88bc
starforce333 <starforce@i.ua>
2018-12-26 10:49:48 +0200

    fix

---
+++ src/socket/Group.java
@@ -0,0 +1,33 @@
+package socket;
+
+import java.util.ArrayList;
+
+public class Group {
+    private String name;
+    private ArrayList<Student> al = new ArrayList<>();
+
+    public Group(String name) {
+        super();
+        this.name = name;
+    }
+
+    public void addStudent(Student a) {
+        al.add(a);
+    }
+
+    public Student[] getGroup() {
+        Student[] starr = new Student[al.size()];
+        for (int i = 0; i < al.size(); i++) {
+            starr[i] = al.get(i);
+        }
+        return starr;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/socket/Main.java
@@ -0,0 +1,36 @@
+package socket;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Main {
+    public static void main(String[] args) {
+        String ansver = "";
+        Group gr = new Group("PN121");
+        gr.addStudent(new Student("Ivanov", "Ivan", 3));
+        gr.addStudent(new Student("Petrov", "Petr", 4));
+        gr.addStudent(new Student("Sidorov", "Sidr", 5));
+        gr.addStudent(new Student("Garbuzov", "Grbuz", 1));
+        ansver += "<html><head><title>Student</title> <meta charset='utf-8'></head><body><p>Список студентов группы: "
+                + gr.getName() + "</p><br>";
+        ansver += "<table border='2' cellpadding='5' ><tr><th>Фамилия</th><th>Имя</th><th>Курс</th></tr>";
+        Student[] starr = gr.getGroup();
+        for (int i = 0; i < starr.length; i++) {
+            ansver += "<tr>";
+            ansver += "<td>" + starr[i].getName() + "</td>";
+            ansver += "<td>" + starr[i].getLastname() + "</td>";
+            ansver += "<td>" + starr[i].getCourse() + "</td>";
+            ansver += "</tr>";
+        }
+        ansver += "</table></body></html>";
+        try (ServerSocket soc = new ServerSocket(8080)) {
+            for (; ; ) {
+                Socket clisoc = soc.accept();
+                Client cli = new Client(clisoc, ansver);
+            }
+        } catch (IOException e) {
+            System.out.println("Error to server Socket Open!!!");
+        }
+    }
+}
